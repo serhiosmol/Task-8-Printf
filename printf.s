49abaf5357912fc4d674b2744d8417625b8a2263
Sergey <serhiosmol@gmail.com>
2026-03-20 19:29:59 +0300

    added more comments and started %s processing

--- printf.s
+++ printf.s
@@ -1,30 +1,30 @@
 section .data
-	fmt_string	db "My name is %c%c%c%c%c%c", 0x0a
+	fmt_string	db "My name is %s", 0x0a
+	string_to_print	db "Sergey"
 
 section .text
 	global _start
 
 _start:
 	mov rdi, fmt_string
-	mov rsi, 'S'
-	mov rdx, 'e'
-	mov rcx, 'r'
-	mov r8, 'g'
-	mov r9, 'e'
-	push 'y'
+	; push 'y'
+	; mov r9, 'e'
+	; mov r8, 'g'
+	; mor rcx, 'r'
+	; mov rdx, 'e'
+	mov rsi, string_to_print
 	call my_printf
-	add rsp, 8
-
-	mov rax, 0x3c
-	xor rdi, rdi
+	; add rsp, 8 	; because there was one argument in stack
+	
+	mov rax, 0x3c	; syscall exit code 
+	xor rdi, rdi	
 	syscall
 
 ; rax - return code
 ; args: rdi rsi rdx rcx r8 r9 stack
-; unused: rbx rcx r11 (NO! mov r11, RFLAGS) r12 r13 r14 r15
 
 ; my_printf(const char* format, ...)
-; rdi - ptr on fmt_string, rsi - expected char
+; rdi - ptr on fmt_string
 ; rbx - address for loop (string), r12 - changing rsp
 my_printf:
 	push r9
@@ -35,53 +35,55 @@ my_printf:
 
 	; after that stack looks like that
 	; (registers mean chars in them):
-	; rsi rdx rcx r8 r9 RETURN_ADDRESS 'y' ...
-	; /\
-	; rsp in the beginning
+	; rsi rdx rcx r8  r9 RETURN_ADDRESS 'y' ...
+	;  |   |   |   |   |       |         |
+	; rsp rsp rsp rsp rsp     rsp       rsp
+	;      +   +   +   +       +         +
+	;      8   16  24  32      40        48
 	; we don't need to return symbols to registers
 	; so we will just jump over return address
 	; and change rsp in the end of executing function
 
-	mov rbx, rdi
-	mov r12, rsp
-	mov r14, 0 ; counter of %c-s
+	mov rbx, rdi	; because rdi is used for syscalls
+	mov r12, rsp	; changing rsp is kinda like shit
+	mov r14, 0 	; counter of %
 .loop:
-	mov al, [rbx]
-	cmp al, 0
+	mov al, [rbx]	; move symbol of format string to 1 byte register
+	cmp al, 0	; is it the end of string?
 	jz .end_of_string
 	
-	cmp al, '%'
+	cmp al, '%'	; is it %?
 	jne .skip_call
 
-	inc r14
+	inc r14		; increase count of %
 
-	mov rax, 0x1
-	mov rdi, 1
-	mov rsi, r12
-	mov rdx, 1
+	mov rax, 0x1	; syscall write code
+	mov rdi, 1	; stdout
+	mov rsi, r12	; pointer to symbol from arguments
+	mov rdx, 1	; count of writing symbols
 	syscall
 
-	add r12, 8
+	add r12, 8	; come to next argument in stack
 
-	cmp r14, 5
+	cmp r14, 5	; 5 arguments are given in registers and pushed after calling
 	jne .skip_jump_over_ret
-	add r12, 8
+	add r12, 8	; jump over return address
 
 .skip_jump_over_ret:
-	add rbx, 2
-	jmp .loop
+	add rbx, 2	; skip '%' and symbol after it
+	jmp .loop	; continue string processing
 
 .skip_call:
-	mov rax, 0x1
-	mov rdi, 1
-	mov rsi, rbx
-	mov rdx, 1
+	mov rax, 0x1	; syscall write code
+	mov rdi, 1	; stdout
+	mov rsi, rbx	; pointer on printing buffer
+	mov rdx, 1	; count of writing symbols
 	syscall
 
-	inc rbx
-	jmp .loop
+	inc rbx		; go to next symbol in format string
+	jmp .loop	; continue string processing
 
 .end_of_string:
-	add rsp, 40
+	add rsp, 40	; return stack pointer to return address
 	ret
 
