02d06f9007445b2c5999b5b12d8655a601872879
Sergey <serhiosmol@gmail.com>
2026-04-02 22:26:31 +0300

    added normal %c, %s and C compatibility

--- printf.s
+++ printf.s
@@ -1,24 +1,25 @@
-section .data
-	fmt_string	db "My name is %s%c", 0x0a, 0
-	str1		db "Sergey", 0
+global my_printf
+; global _start
+
+section .bss
+	output_buffer	resb 256
+	; dec_buffer	resb 12
+
+; section .data
+	; fmt_string	db "My name is %s%c", 0x0a, 0
+	; str1		db "Sergey", 0
 
 section .text
-	global _start
 
-_start:
-	mov rdi, fmt_string
-	; push 'y'
-	; mov r9, 'e'
-	; mov r8, 'g'
-	; mor rcx, 'r'
-	mov rdx, '!'
-	mov rsi, str1
-	call my_printf
-	; add rsp, 8 	; because there was one argument in stack
-	
-	mov rax, 0x3c	; syscall exit code 
-	xor rdi, rdi	
-	syscall
+; _start:
+	; mov rdi, fmt_string
+	; mov rdx, '!'
+	; mov rsi, str1
+	; call my_printf
+
+	; mov rax, 0x3c	; syscall exit code
+	; xor rdi, rdi
+	; syscall
 
 ; rax - return code
 ; args: rdi rsi rdx rcx r8 r9 stack
@@ -33,67 +34,68 @@ my_printf:
 	push rdx
 	push rsi
 
-	; char processing:
-	; after that stack looks like that
-	; (registers mean chars in them):
-	; rsi rdx rcx r8  r9 RETURN_ADDRESS 'y' ...
-	;  |   |   |   |   |       |         |
-	; rsp rsp rsp rsp rsp     rsp       rsp
-	;      +   +   +   +       +         +
-	;      8   16  24  32      40        48
-	; we don't need to return symbols to registers
-	; so we will just jump over return address
-	; and change rsp in the end of executing function
-
 	mov rbx, rdi	; because rdi is used for syscalls
-	mov r12, rsp	; changing rsp is kinda like shit
-	xor r14, r14 	; counter of %
+	lea r12, [rsp]
+	lea r13, [rel output_buffer] 	; ptr on symbol in output_buffer
+	xor r14, r14	; counter of %
 .loop:
 	mov al, [rbx]	; move symbol of format string to 1 byte register
-	cmp al, 0	; is it the end of string?
+	test al, al	; is it the end of string?
 	jz .end_of_string
-	
+
 	cmp al, '%'	; is it %?
 	jne .skip_call
 
 	inc rbx
 	mov al, [rbx]	; we need to check symbol after %
-	cmp al, 'c'	
+	cmp al, 'c'
 	je .processing_char	; going to process char (%c)
 	cmp al, 's'
 	je .processing_string	; going to process string (%s)
+	; cmp al, 'd'
+	; je .processing_dec
 	jmp .skip_call	; skip processing if the format is unknown
 
 .processing_char:
-	mov rax, 0x1	; syscall write code
-	mov rdi, 1	; stdout
-	mov rsi, r12	; pointer to symbol from arguments
-	mov rdx, 1	; count of writing symbols
-	syscall
-	
+	mov rax, [r12]
+	mov byte [r13], al
+	inc r13
+
 	add r12, 8	; going to next argument in stack
 	inc r14		; increase count of wrote arguments
 	jmp .after_processing
 
 .processing_string:
-	mov rsi, [r12]	; in stack it's like char** str, so here rsi = *str
-	mov rcx, 0	; rcx - length of given argument string
+	mov rsi, [r12]	; in stack it's like char** str, so here char* rsi = *str
 
-.count_length:
-	cmp byte [rsi + rcx], 0	; is it the end of string (is rcx length)?
-	je .got_length	
-	inc rcx		; increase length if it is not the end of string
-	jmp .count_length	; if rcx is less than real length, continue
+.copy_string:
+	mov al, byte [rsi]
+	test al, al
+	je .done_copy
 
-.got_length:
-	mov rax, 0x1	; syscall write code
-	mov rdi, 1	; stdout
-	mov rdx, rcx	; length of string
-	syscall
+	mov [r13], al
+	inc r13
+	inc rsi
+	jmp .copy_string
 
+.done_copy:
 	add r12, 8	; going to next argument in stack
 	inc r14		; increase count of wrote arguments
 	jmp .after_processing
+
+; .processing_dec:
+	; mov eax, [r12]
+	; cmp rax, 0
+	; jge .positive
+
+	; mov byte [r13], '-'
+	; inc r13
+	; neg rax
+
+; .positive:
+	; xor edx, edx 	;
+	; mov di, 10
+	; div di
 
 .after_processing:
 	cmp r14, 5	; 5 arguments are given in registers and pushed after calling
@@ -105,16 +107,25 @@ my_printf:
 	jmp .loop	; continue string processing
 
 .skip_call:
-	mov rax, 0x1	; syscall write code
-	mov rdi, 1	; stdout
-	mov rsi, rbx	; pointer on printing buffer
-	mov rdx, 1	; count of writing symbols
-	syscall
-
+	mov byte [r13], al	; add symbol to output buffer
+	inc r13		; go to next symbol in output buffer
 	inc rbx		; go to next symbol in format string
 	jmp .loop	; continue string processing
 
 .end_of_string:
-	add rsp, 40	; return stack pointer to return address
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [rel output_buffer]
+	mov rdx, r13
+	sub rdx, rsi
+	syscall
+
+	pop rsi
+	pop rdx
+	pop rcx
+	pop r8
+	pop r9
+
+	xor rax, rax
 	ret
 
